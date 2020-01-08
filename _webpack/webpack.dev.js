cd311e9b37003114901e9347b4fe4323049af980
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-01-08 21:31:24 +0000

    feat: day5

---
+++ _webpack/webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+};
