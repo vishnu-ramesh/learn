dc57243c4ec36399deffe3891c58a03935f0c37c
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-01-10 21:45:08 +0000

    feat: day5

--- _webpack/webpack.dev.js
+++ _webpack/webpack.dev.js
@@ -1,23 +1,6 @@
-const path = require('path');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common');
 
-module.exports = {
-  entry: {
-    app: './src/client/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, '../_dist'),
-    filename: 'my-first-webpack.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
+module.exports = merge(commonConfig, {
   mode: 'development',
-};
+});
